fb3977a4cf8007de4222a28e31e0938abb2cff82
jianchau <ajianchau@gmail>
2022-10-26 11:06:48 +0800

    router:add react router 6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 /** @format */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '@/modules/app';
+import ErrorPage from './error-page';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>,
+);
